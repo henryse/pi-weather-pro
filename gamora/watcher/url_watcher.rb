7f277caa112016bd91b81e2a7d3671b6d57b8b4b
henryse <henryse@hotwire.com>
2016-09-13 20:35:24 -0700

    Remove .idea file and updated config.yml

--- gamora/watcher/url_watcher.rb
+++ gamora/watcher/url_watcher.rb
@@ -1,27 +1,42 @@
 require 'net/http'
+require 'logger'
 
 class UrlWatcher
-  def initialize(sleep,url)
+  def initialize(sleep,url,execute)
     @sleep = sleep
     @url = url
+    @execute = execute
+
+    @logger = Logger.new(STDOUT)
+    @logger.level = Logger::WARN
   end
 
   def test
+    success = false
     begin
       uri = URI('https://www.google.com')
-      return Net::HTTP.get_response(uri).code.to_i == 200
-    rescue
-      puts 'ouch'
+
+      success = Net::HTTP.get_response(uri).code.to_i == 200
+
+      unless success
+        @logger.error("Unable to find uri: #{uri.to_s}")
+      end
+    rescue Exception => e
+      @logger.error("Unable to connect: #{e}")
     end
 
-    false
+    success
   end
 
   def execute
     while true
       unless test
-        puts 'call the command'
+        @logger.warn("Executing command #{@execute} because of a test failure.")
+        unless system(@execute)
+          @logger.error("Failed to #{@execute}.")
+        end
       end
+
       sleep(@sleep)
     end
   end
--- gamora/watcher/watcher.rb
+++ gamora/watcher/watcher.rb
@@ -1,21 +1,61 @@
 require 'yaml'
 require 'fileutils'
-require 'rest-client'
+require 'logger'
+require 'ostruct'
+require 'optparse'
 require_relative 'url_watcher'
 
 class Watcher
   def initialize(config_file)
+    @logger = Logger.new(STDOUT)
+    @logger.level = Logger::WARN
+
+    # Load the config file
     @config = YAML.load_file("#{Dir.pwd}/#{config_file}")
-    @threads = []
+
+    # Make sure all is well...
     @config.each do |key,value|
-      unless value['url'].nil?
-        @threads << Thread.new do
-          UrlWatcher.new(value['sleep'],value['url']).execute
-        end
+      if  value['url'].nil?
+        throw  "Missing url for #{key}"
+      end
+      if  value['sleep'].nil?
+        throw "Missing sleep for #{key}"
+      end
+      if  value['execute'].nil?
+        throw "Missing execute for #{key}"
       end
     end
+
+    @threads = []
+
+    @config.each do |key,value|
+      @logger.info("Setting up #{key}")
+      @threads << Thread.new do
+        UrlWatcher.new(value['sleep'],value['url'], value['execute']).execute
+      end
+    end
+  end
+
+  def watch
     @threads.each { |thr| thr.join}
   end
 end
 
-Watcher.new("yamltest.yml")
+options = OpenStruct.new
+
+opt_parser = OptionParser.new do |opts|
+  opts.banner = 'Usage: watcher.rb [options]'
+
+  opts.on('--config=CONFIG', String, 'URL Weather Pro endpoint.') do |config|
+    options.config = config
+  end
+end
+
+opt_parser.parse!
+
+if options.config.nil?
+  puts 'ERROR: You need to pass at lest an YAML config file.'
+  puts opt_parser.to_s
+else
+  Watcher.new(options.config).watch
+end
