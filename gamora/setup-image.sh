3509a9c645272eacce1137ae23a7f20f367e2c84
henryse <henryse@hotwire.com>
2016-09-18 21:57:55 -0700

    Fixed issue with --host

--- gamora/setup-image.sh
+++ gamora/setup-image.sh
@@ -8,17 +8,16 @@ sudo apt-get install -y libgdbm-dev libreadline6-dev libncurses5-dev
 sudo apt-get install -y libpq-dev libffi-dev
 sudo apt-get install -y vim
 
+git clone git://git.drogon.net/wiringPi
+cd wiringPi
+git pull origin
+./build
+
+# Need to document downloading ruby here.
 cd /home/pi/ruby/ruby-2.3.1
 ./configure
 make clean
 make
 sudo make install
-
-cd /home/pi/python3/Python-3.5.2
-./configure
-make clean
-make
-sudo make install
-
 sudo gem update
-sudo gem install bundle sinatra thin rack sqlite3 rest-client
+sudo gem install bundle sinatra thin rack sqlite3 rest-client wiringpi
