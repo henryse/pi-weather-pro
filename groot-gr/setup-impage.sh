ad49745c43d1df300a0dfbc07a76397f54bd4977
henryse <henryse@hotwire.com>
2016-09-29 09:07:03 -0500

    ok added new directory for groot-jr

--- groot-gr/setup-impage.sh
+++ groot-gr/setup-impage.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-
 sudo apt-get -y update
-sudo apt-get install -y make g++
+sudo apt-get install -y make g++ vim
 sudo apt-get install -y curl git-core python-software-properties
 sudo apt-get install -y build-essential zlib1g-dev libyaml-dev libssl-dev
 sudo apt-get install -y libgdbm-dev libreadline6-dev libncurses5-dev
 sudo apt-get install -y libpq-dev libffi-dev
 sudo apt-get install -y vim i2c-tools sqlite3
+
 
 git clone git://git.drogon.net/wiringPi
 cd wiringPi
@@ -14,7 +14,7 @@ git pull origin
 ./build
 
 # Need to document downloading ruby here.
-cd /home/pi/ruby/ruby-2.3.1
+cd /home/pi/ruby-2.3.1
 ./configure
 make clean
 make
