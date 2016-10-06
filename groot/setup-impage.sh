571cfc06078f3125bfa4564bba8b5c0d5a151b26
henryse <henryse@hotwire.com>
2016-10-05 20:01:50 -0700

    Added Groot Setup

--- groot/setup-impage.sh
+++ groot/setup-impage.sh
@@ -9,16 +9,16 @@ sudo apt-get install -y vim i2c-tools sqlite3 libi2c-dev
 sudo apt-get install -y python-all-dev  build-essential python-dev python-pip
 
 
-git clone git://git.drogon.net/wiringPi
-cd wiringPi
-git pull origin
-./build
+# git clone git://git.drogon.net/wiringPi
+# cd wiringPi
+# git pull origin
+# ./build
 
 # Need to document downloading ruby here.
-cd /home/pi/ruby-2.3.1
-./configure
-make clean
-make
-sudo make install
+# cd /home/pi/ruby-2.3.1
+# ./configure
+# make clean
+# make
+# sudo make install
 sudo gem update
 sudo gem install bundle sinatra thin rack sqlite3 rest-client wiringpi
