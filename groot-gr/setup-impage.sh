8059775c12dffc47769473e5e81d965588e1ed38
henryse <henryse@hotwire.com>
2016-10-05 22:29:49 -0700

    Cleaned up setup

--- groot-gr/setup-impage.sh
+++ groot-gr/setup-impage.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
-sudo apt-get -y update
-sudo apt-get install -y make g++ vim
-sudo apt-get install -y curl git-core python-software-properties
-sudo apt-get install -y build-essential zlib1g-dev libyaml-dev libssl-dev
-sudo apt-get install -y libgdbm-dev libreadline6-dev libncurses5-dev
-sudo apt-get install -y libpq-dev libffi-dev
-sudo apt-get install -y vim i2c-tools sqlite3 libi2c-dev
-sudo apt-get install -y python-all-dev  build-essential python-dev python-pip
 
+sudo apt-get -y update
+sudo apt-get install -y build-essential
+sudo apt-get install -y libpq-dev libffi-dev zlib1g-dev libyaml-dev libssl-dev
+sudo apt-get install -y libgdbm-dev libreadline6-dev libncurses5-dev
+sudo apt-get install -y i2c-tools sqlite3 libi2c-dev
+sudo apt-get install -y make g++ vim wget curl git-core
+sudo apt-get install -y python-all-dev python-software-properties
 
 git clone git://git.drogon.net/wiringPi
 cd wiringPi
 git pull origin
 ./build
 
-# Need to document downloading ruby here.
+cd ~
+wget https://cache.ruby-lang.org/pub/ruby/2.3/ruby-2.3.1.tar.gz
+tar -xvf  ruby-2.3.1.tar.gz
 cd /home/pi/ruby-2.3.1
 ./configure
 make clean
